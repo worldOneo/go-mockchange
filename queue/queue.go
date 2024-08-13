169631ed966e3c6026eefab6e0b791eed5f87400
worldOneo <62426504+worldOneo@users.noreply.github.com>
2024-08-13 14:14:52 +0200

    WIP: Fix queue writer & reader + allocations are for poor people

--- queue/queue.go
+++ queue/queue.go
@@ -14,19 +14,20 @@ import (
 // QueueWriter is the pushing side of a queue
 // it can only write
 type QueueWriter[T comparable] struct {
-	fd            int
 	currentOffset int64
 	totalOffset   int64
-	mmapedRegion  []byte
-	entrySize     int
+	fd            int
 	runningCount  int
-	syncStrategy  SyncStrategy[T]
+	entrySize     int
 	pagesize      int
+	syncStrategy  SyncStrategy[T]
+	mmapedRegion  []byte
+	tmp           queueEntry[T]
 }
 
 type queueEntry[T comparable] struct {
-	entry    T
-	metadata uint32
+	entry  T
+	status uint32
 }
 
 type checkedQueueEntry[T comparable] struct {
@@ -39,13 +40,13 @@ type SyncStrategy[T comparable] interface {
 	Sync(fd int, region []byte) fasterror.Error
 }
 
-// Create initializes a new queue backed by the file
+// Writer initializes a new queue backed by the file
 //
-// This function overwrites the file so calling Create on a file
+// This function overwrites the file so calling Writer on a file
 // after recovery will lose the content
 //
 // runnningCount determins the amounts of entries in memory
-func Create[T comparable](file string, runningCount int64, syncStrategy SyncStrategy[T]) (QueueWriter[T], error) {
+func Writer[T comparable](file string, runningCount int64, syncStrategy SyncStrategy[T]) (QueueWriter[T], error) {
 	var queue QueueWriter[T]
 	// open backing storage
 	fd, err := unix.Open(file, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o666)
@@ -87,6 +88,8 @@ func Create[T comparable](file string, runningCount int64, syncStrategy SyncStra
 
 var koopman = crc32.MakeTable(crc32.Koopman)
 
+type QueueStatus = uint32
+
 const (
 	// recovery on prepublished fields are considered published
 	// prepublished marks that at the time of crash a sync wasn't
@@ -95,27 +98,71 @@ const (
 	//
 	// PrePublished entries are to be replayed as published on
 	// recovery
-	QueueEntryStatusPrePublished = 1
+	EntryStatusPrePublished QueueStatus = 1
 	// the value is accessable for consumers and has been synced
 	// in accordance with the sync strategy.
 	//
 	// Published entries are to be replayed on recovery
-	QueueEntryStatusPublished = 2
+	EntryStatusPublished QueueStatus = 2
 	// the consumer has started work on this entry
 	// because this queue has no transactions
 	// the consumer might crash before finalizing
 	// its work. Recovery must be handled appropriately.
-	QueueEntryStatusConsuming = 3
+	EntryStatusConsuming QueueStatus = 3
 	// the entry has been completly consumed and no replay
 	// is necessary
-	QueueEntryStatusConsumed = 4
+	EntryStatusConsumed QueueStatus = 4
 	// upon reading a value its integrity couldn't be
 	// ensured with error checking code
 	//
 	// this flag is virtual and not stored only replayed
 	// no data of a corrupted entry can be trusted
-	QueueEntryStatusCorrupted = 5
+	EntryStatusCorrupted QueueStatus = 5
 )
+
+func moveFdMapping(fd int, oldRegion []byte, regionSize int64, totalOffset int64, pageSize int, ftrunc bool) (int64, []byte, fasterror.Error) {
+	var data []byte
+	err := unix.Msync(oldRegion, unix.MS_ASYNC)
+	if err != nil {
+		return 0, data, fasterror.Create("Failed to async msync mmaped region for new allocation")
+	}
+
+	if ftrunc {
+		err = unix.Ftruncate(fd, totalOffset+regionSize*2)
+		if err != nil {
+			return 0, data, fasterror.Create("Failed to extended storage file via ftruncate")
+		}
+	}
+
+	// page size math
+	desiredOffset := totalOffset + int64(regionSize)
+	pageCountBelowOffset := desiredOffset / int64(pageSize)
+	pageBasedOffset := pageCountBelowOffset * int64(pageSize)
+	pageDesiredOffsetDelta := desiredOffset - pageBasedOffset
+
+	newRegion, err := unix.Mmap(
+		fd,
+		pageBasedOffset,
+		int(regionSize)+int(pageDesiredOffsetDelta),
+		unix.PROT_READ|unix.PROT_WRITE,
+		unix.MAP_SHARED,
+	)
+	if err != nil {
+		fmt.Printf("Err: %v\n", err)
+		return 0, data, fasterror.Create("Failed to create extended mmap")
+	}
+
+	err = unix.Munmap(oldRegion)
+	if err != nil {
+		err = unix.Munmap(newRegion)
+		if err != nil {
+			return 0, data, fasterror.Create("Failed to unmap new region after failing to unmap old region after extending")
+		}
+		return 0, data, fasterror.Create("Failed to unmap the old region after extending")
+	}
+
+	return pageDesiredOffsetDelta, newRegion, fasterror.Nil()
+}
 
 // Write writes a new entry to the queue
 //
@@ -136,61 +183,34 @@ func (queue *QueueWriter[T]) Write(t T) fasterror.Error {
 	}
 
 	if queue.currentOffset >= int64(len(queue.mmapedRegion)) {
-		regionSize := queue.runningCount * queue.entrySize
-
-		err := unix.Msync(queue.mmapedRegion, unix.MS_ASYNC)
-		if err != nil {
-			return fasterror.Create("Failed to async msync mmaped region for new allocation")
-		}
-
-		err = unix.Ftruncate(queue.fd, queue.totalOffset+int64(regionSize*2))
-		if err != nil {
-			return fasterror.Create("Failed to extended storage file via ftruncate")
-		}
-
-		// page size math
-		desiredOffset := queue.totalOffset+int64(regionSize)
-		pageCountBelowOffset := desiredOffset / int64(queue.pagesize)
-		pageBasedOffset := pageCountBelowOffset * int64(queue.pagesize)
-		pageDesiredOffsetDelta := desiredOffset - pageBasedOffset
-
-		newRegion, err := unix.Mmap(
+		regionSize := int64(queue.entrySize) * int64(queue.runningCount)
+		delta, newRegion, err := moveFdMapping(
 			queue.fd,
-			pageBasedOffset,
-			int(regionSize)+int(pageDesiredOffsetDelta),
-			unix.PROT_READ|unix.PROT_WRITE,
-			unix.MAP_SHARED,
+			queue.mmapedRegion,
+			regionSize,
+			queue.totalOffset,
+			queue.pagesize,
+			true,
 		)
-		if err != nil {
-			fmt.Printf("Err: %v\n", err)
-			return fasterror.Create("Failed to create extended mmap")
+		if !err.IsNil() {
+			return err
 		}
-
-		err = unix.Munmap(queue.mmapedRegion)
-		if err != nil {
-			err = unix.Munmap(newRegion)
-			if err != nil {
-				return fasterror.Create("Failed to unmap new region after failing to unmap old region after extending")
-			}
-			return fasterror.Create("Failed to unmap the old region after extending")
-		}
-
-		queue.totalOffset += int64(regionSize)
-		queue.currentOffset = pageDesiredOffsetDelta
+		queue.totalOffset += regionSize
+		queue.currentOffset = delta
 		queue.mmapedRegion = newRegion
 	}
 
-	entry := queueEntry[T]{
-		entry:    t,
-		metadata: QueueEntryStatusPrePublished,
+	queue.tmp = queueEntry[T]{
+		entry:  t,
+		status: EntryStatusPrePublished,
 	}
 	// prepare publish
-	uptr := unsafe.Pointer(&entry.entry)
-	slice := unsafe.Slice((*byte)(uptr), unsafe.Sizeof(entry.entry))
+	uptr := unsafe.Pointer(&queue.tmp)
+	slice := unsafe.Slice((*byte)(uptr), unsafe.Sizeof(queue.tmp))
 	crc := crc32.Checksum(slice, koopman)
 	ptr := (*checkedQueueEntry[T])(unsafe.Pointer(&queue.mmapedRegion[queue.currentOffset]))
 	ptr.entry = t
-	atomic.StoreUint64(&ptr.statusCrc32, (uint64(QueueEntryStatusPrePublished)<<32)+uint64(crc))
+	atomic.StoreUint64(&ptr.statusCrc32, (uint64(EntryStatusPrePublished)<<32)+uint64(crc))
 	queue.currentOffset += int64(queue.entrySize)
 
 	// sync
@@ -200,8 +220,167 @@ func (queue *QueueWriter[T]) Write(t T) fasterror.Error {
 	}
 
 	// ready to consume
-	entry.metadata = QueueEntryStatusPublished
+	queue.tmp.status = EntryStatusPublished
 	crc = crc32.Checksum(slice, koopman)
-	atomic.StoreUint64(&ptr.statusCrc32, (uint64(QueueEntryStatusPublished)<<32)+uint64(crc))
+	atomic.StoreUint64(&ptr.statusCrc32, (uint64(EntryStatusPublished)<<32)+uint64(crc))
 	return fasterror.Nil()
 }
+
+// QueueReader is the pulling side of a queue
+// it can only write
+type QueueReader[T comparable] struct {
+	currentOffset int64
+	totalOffset   int64
+	fd            int
+	entrySize     int
+	runningCount  int
+	pagesize      int
+	syncStrategy  SyncStrategy[T]
+	mmapedRegion  []byte
+	fstat         unix.Stat_t
+	tmp           queueEntry[T]
+	recovery      bool
+}
+
+// Reader initializes a new reader backed by the file
+//
+// in recovery mode reader will also produce entries
+// with the status QueueEntryStatusPrePublished
+//
+// runningCount must match the Writers running count
+func Reader[T comparable](file string, runningCount int64, syncStrategy SyncStrategy[T], recovery bool) (QueueReader[T], error) {
+	var queue QueueReader[T]
+	// open backing storage
+	fd, err := unix.Open(file, os.O_RDWR, 0o666)
+	if err != nil {
+		return queue, fmt.Errorf("failed to open persisted file: %v", err)
+	}
+
+	var t checkedQueueEntry[T]
+	var entrySize = unsafe.Sizeof(t)
+	var memSize = entrySize * uintptr(runningCount)
+
+	// mapping file to writable region
+	region, err := unix.Mmap(fd, 0, int(memSize), unix.PROT_READ|unix.PROT_WRITE, unix.MAP_SHARED)
+	if err != nil {
+		err2 := unix.Close(fd)
+		if err2 != nil {
+			return queue, fmt.Errorf("failed to close queue fd after failing to mmap the fd: %v; initial error was: %v", err2, err)
+		}
+		return queue, fmt.Errorf("failed to mmap the fd: %v", err)
+	}
+
+	// check if data is present
+	err = unix.Fstat(fd, &queue.fstat)
+	if err != nil {
+		err2 := unix.Munmap(region)
+		err3 := unix.Close(fd)
+		if err2 != nil && err3 != nil {
+			return queue, fmt.Errorf("failed to unmap region: %v and closing the fd: %v after failing fstat: %v", err2, err3, err)
+		}
+		if err2 != nil {
+			return queue, fmt.Errorf("failed to unmap region: %v after failing fstat: %v", err2, err)
+		}
+		if err3 != nil {
+			return queue, fmt.Errorf("failed to close the fd: %v after failing fstat: %v", err3, err)
+		}
+		return queue, err
+	}
+
+	queue.fd = fd
+	queue.mmapedRegion = region
+	queue.entrySize = int(entrySize)
+	queue.runningCount = int(runningCount)
+	queue.currentOffset = 0
+	queue.syncStrategy = syncStrategy
+	queue.totalOffset = 0
+	queue.pagesize = os.Getpagesize()
+	queue.recovery = recovery
+
+	return queue, nil
+}
+
+// Read reads data from the queue marking the first
+// readable entry as consuming
+//
+// When hitting a corrupt entry the data isn't modified
+// and the reader cannot continue
+func (queue *QueueReader[T]) Read() (T, QueueStatus, fasterror.Error) {
+	for {
+		var t T
+		status := EntryStatusCorrupted
+		regionSize := int64(queue.entrySize) * int64(queue.runningCount)
+		for queue.fstat.Size < queue.totalOffset+regionSize {
+			err := unix.Fstat(queue.fd, &queue.fstat)
+			if err != nil {
+				return t, status, fasterror.Create("Failed to fstat queue file")
+			}
+		}
+
+		if queue.currentOffset >= int64(len(queue.mmapedRegion)) {
+			delta, newRegion, err := moveFdMapping(
+				queue.fd,
+				queue.mmapedRegion,
+				regionSize,
+				queue.totalOffset,
+				queue.pagesize,
+				false,
+			)
+			if !err.IsNil() {
+				return t, status, err
+			}
+			queue.totalOffset += regionSize
+			queue.currentOffset = delta
+			queue.mmapedRegion = newRegion
+		}
+
+		memPtr := &queue.mmapedRegion[queue.currentOffset]
+		ptr := (*checkedQueueEntry[T])(unsafe.Pointer(memPtr))
+
+		for {
+			statusCrc := atomic.LoadUint64(&ptr.statusCrc32)
+			status := uint32(statusCrc >> 32)
+			receivedCrc := uint32(statusCrc & ((1 << 32) - 1))
+
+			queue.tmp = queueEntry[T]{
+				entry:  ptr.entry,
+				status: status,
+			}
+
+			entryPtr := unsafe.Pointer(&queue.tmp)
+			entryDataSlice := unsafe.Slice((*byte)(entryPtr), unsafe.Sizeof(queue.tmp))
+			checkedCrc := crc32.Checksum(entryDataSlice, koopman)
+
+			if receivedCrc != checkedCrc {
+				return queue.tmp.entry, EntryStatusCorrupted, fasterror.Nil()
+			}
+			if status == EntryStatusConsumed {
+				break
+			}
+
+			statusReadableRecovery := status == EntryStatusPrePublished || status == EntryStatusConsuming
+
+			if (statusReadableRecovery && queue.recovery) || (status == EntryStatusPublished) {
+				queue.tmp.status = EntryStatusConsuming
+				consumingCrc := crc32.Checksum(entryDataSlice, koopman)
+				consumingStatusCrc := (uint64(EntryStatusConsuming) << 32) + uint64(consumingCrc)
+				atomic.StoreUint64(&ptr.statusCrc32, consumingStatusCrc)
+				err := queue.syncStrategy.Submit(queue.tmp.entry)
+				if !err.IsNil() {
+					return t, status, err
+				}
+				err = queue.syncStrategy.Sync(queue.fd, queue.mmapedRegion)
+				if !err.IsNil() {
+					return t, status, err
+				}
+				queue.currentOffset += int64(queue.entrySize)
+				return queue.tmp.entry, status, fasterror.Nil()
+			}
+		}
+	}
+}
+
+// FinishRead marks the current entry as Consumed
+// and syncs that change in accordance with the sync strategy
+// func (queue *QueueReader[T]) Read() (T, QueueStatus, fasterror.Error) {
+// }
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -2,12 +2,13 @@ package queue
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
 func TestQueueWriter_write(t *testing.T) {
 	dir := t.TempDir()
-	q, err := Create(fmt.Sprintf("%s/tmpfile.q", dir), 100, MAsyncRolling[int](50))
+	q, err := Writer(fmt.Sprintf("%s/tmpfile.q", dir), 100, MAsyncRolling[int](50))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -15,6 +16,76 @@ func TestQueueWriter_write(t *testing.T) {
 		err := q.Write(i)
 		if !err.IsNil() {
 			t.Fatalf("Failed to write %d: %s", i, err.ToString())
+		}
+	}
+}
+
+func TestQueueReader_read(t *testing.T) {
+	dir := t.TempDir()
+	q, err := Writer(fmt.Sprintf("%s/tmpfile.q", dir), 100, MAsyncRolling[int](50))
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < 1_000; i++ {
+		err := q.Write(i)
+		if !err.IsNil() {
+			t.Fatalf("Failed to write %d: %s", i, err.ToString())
+		}
+	}
+
+	r, err := Reader(fmt.Sprintf("%s/tmpfile.q", dir), 100, MAsyncRolling[int](50), false)
+	for i := 0; i < 1_000; i++ {
+		entry, status, err := r.Read()
+		if !err.IsNil() {
+			t.Fatalf("Failed to read back %d: %s", i, err.ToString())
+		}
+		if status != EntryStatusPublished {
+			t.Fatalf("Failed to read back, mismatched status %d: %v, v=%v", i, status, entry)
+		}
+		if entry != i {
+			t.Fatalf("Invalid data readback %d: %d", i, entry)
+		}
+	}
+}
+
+func BenchmarkQueueWriter(b *testing.B) {
+	dir := b.TempDir()
+	q, err := Writer(fmt.Sprintf("%s/tmpfile.q", dir), 1000, MAsyncRolling[int](1000))
+	b.ResetTimer()
+	b.ReportAllocs()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		err := q.Write(i)
+		if !err.IsNil() {
+			log.Fatalf(err.ToString())
+		}
+	}
+}
+
+func BenchmarkQueueReader(b *testing.B) {
+	dir := b.TempDir()
+	q, err := Writer(fmt.Sprintf("%s/tmpfile.q", dir), 1000, MAsyncRolling[int](1000))
+	if err != nil {
+		b.Fatal(err)
+	}
+	
+	for i := 0; i < b.N; i++ {
+		err := q.Write(i)
+		if !err.IsNil() {
+			log.Fatalf(err.ToString())
+		}
+	}
+	b.ResetTimer()
+	b.ReportAllocs()
+	r, err := Reader(fmt.Sprintf("%s/tmpfile.q", dir), 1000, MAsyncRolling[int](1000), false)
+
+	for i := 0; i < b.N; i++ {
+		_, _, err := r.Read()
+		if !err.IsNil() {
+			log.Fatalf(err.ToString())
 		}
 	}
 }
